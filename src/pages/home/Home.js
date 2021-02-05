30c057f006fb6e8ab31e4b5db91c0c39ead9ef78
handrkanda <handrykanda@gmail.com>
2021-02-05 16:28:45 +0200

    Banner image

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <div className="Home-container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
