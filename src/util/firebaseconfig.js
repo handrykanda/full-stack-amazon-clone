1a7cbf2100b8c0722e125cac8af8eeaf82d12fcf
handrkanda <handrykanda@gmail.com>
2021-02-05 14:07:04 +0200

    firebase setup

---
+++ src/util/firebaseconfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCUg4XM2-zqpD8Itdq-RuE0X0fakSx8csU",
+  authDomain: "ama-shop.firebaseapp.com",
+  projectId: "ama-shop",
+  storageBucket: "ama-shop.appspot.com",
+  messagingSenderId: "377305178745",
+  appId: "1:377305178745:web:50a2078e317756256beaa8",
+  measurementId: "G-39LGXFQG33",
+};
