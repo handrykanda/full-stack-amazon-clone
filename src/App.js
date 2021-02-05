b79940f20436d7986507b6be5647e55f2d1ce6b7
handrkanda <handrykanda@gmail.com>
2021-02-05 15:34:38 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
