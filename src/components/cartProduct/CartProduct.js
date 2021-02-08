d3be5a5186135d1c2175f092b8510ce7bee5878b
handrkanda <handrykanda@gmail.com>
2021-02-09 01:30:11 +0200

    Making it super responsive

--- src/components/cartProduct/CartProduct.js
+++ src/components/cartProduct/CartProduct.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./CartProduct.css";
 import { useStateValue } from "../../data/StateProvider";
+import Grid from "@material-ui/core/Grid";
 
 function CartProduct({ id, image, title, price, rating, hideButton }) {
   const [{ cart }, dispatch] = useStateValue();
@@ -14,25 +15,30 @@ function CartProduct({ id, image, title, price, rating, hideButton }) {
 
   return (
     <div className="CartProduct">
-      <img className="CartProduct__image" src={image} />
-
-      <div className="CartProduct__info">
-        <p className="CartProduct__title">{title}</p>
-        <p className="CartProduct__price">
-          <small>$</small>
-          <strong>{price}</strong>
-        </p>
-        <div className="CartProduct__rating">
-          {Array(rating)
-            .fill()
-            .map((_, i) => (
-              <p key={i}>🌟</p>
-            ))}
-        </div>
-        {!hideButton && (
-          <button onClick={removeFromCart}>Remove from Cart</button>
-        )}
-      </div>
+      <Grid container alignContent="center" alignItems="center">
+        <Grid item md={5} sm={5} xs={12}>
+          <img className="CartProduct__image" src={image} />
+        </Grid>
+        <Grid item md={7} sm={7} xs={12}>
+          <div className="CartProduct__info">
+            <p className="CartProduct__title">{title}</p>
+            <p className="CartProduct__price">
+              <small>$</small>
+              <strong>{price}</strong>
+            </p>
+            <div className="CartProduct__rating">
+              {Array(rating)
+                .fill()
+                .map((_, i) => (
+                  <p key={i}>🌟</p>
+                ))}
+            </div>
+            {!hideButton && (
+              <button onClick={removeFromCart}>Remove from Cart</button>
+            )}
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- src/components/order/Order.js
+++ src/components/order/Order.js
@@ -3,25 +3,38 @@ import "./Order.css";
 import moment from "moment";
 import CartProduct from "../../components/cartProduct/CartProduct";
 import CurrencyFormat from "react-currency-format";
+import Grid from "@material-ui/core/Grid";
 
 function Order({ order }) {
   return (
     <div className="order">
-      <h2>Order</h2>
-      <p>{moment.unix(order.data.created).format("MMMM Do YYYY, h:mma")}</p>
-      <p className="order__id">
-        Order ID: <small>{order.id}</small>
-      </p>
-      {order.data.cart?.map((item) => (
-        <CartProduct
-          id={item.id}
-          title={item.title}
-          image={item.image}
-          price={item.price}
-          rating={item.rating}
-          hideButton
-        />
-      ))}
+      <Grid container alignContent="center">
+        <Grid item md={8} sm={8} xs={12}>
+          <Grid container alignContent="center">
+            <Grid item md={8} sm={8} xs={12}>
+              <h2>Order</h2>
+              <p>
+                {moment.unix(order.data.created).format("MMMM Do YYYY, h:mma")}
+              </p>
+            </Grid>
+            <Grid item md={4} sm={4} xs={12}>
+              <p>
+                Order ID: <small>{order.id}</small>
+              </p>
+            </Grid>
+          </Grid>
+          {order.data.cart?.map((item) => (
+            <CartProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+              hideButton
+            />
+          ))}
+        </Grid>
+      </Grid>
       <CurrencyFormat
         renderText={(value) => (
           <h3 className="order__total">Order Total: {value}</h3>
